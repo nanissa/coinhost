cb2112dbe36af9650e7da20d068e088a75c2c559
George <george@boost-media.com>
2013-04-05 06:15:37 -0700

    First test passing!

---
+++ spec/requests/pages_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+
+describe 'Render Home Page' do
+  it 'succeeds' do
+    visit '/pages/index'
+    page.should have_content 'Looking to purchase Web Hosting with Bitcoin?'
+  end
+end
