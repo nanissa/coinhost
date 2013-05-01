e1c1f788e5d6d3a5b1ecd4a5daaacb1e0b5b7d92
George <george@boost-media.com>
2013-04-30 20:39:13 -0700

    Remove SECRET config check. ENV not available during slug compilation

--- config/initializers/requirements.rb
+++
@@ -1,8 +0,0 @@
-if ENV[ 'SECRET' ].nil? || ENV[ 'SECRET' ].length < 5
-  begin
-    raise "\n\nSECRET not set properly. App start aborted.\n\n" 
-  rescue => err
-    err.set_backtrace([])
-    fail
-  end
-end
